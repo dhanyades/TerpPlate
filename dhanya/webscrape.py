bff0495595e872255777c32ab3d9fccbcd4f5f8f
Dhanya Desai <dhanyadesai@dhanyas-mbp.umd.edu>
2024-10-26 17:41:24 -0400

    second commit

--- dhanya/webscrape.py
+++ dhanya/webscrape.py
@@ -21,16 +21,38 @@ if response.status_code == 200:
     nutrition_data = {}
 
     nutrients = {
-
-        'Calories per serving' : 'Calories',
         'Total Fat' : 'Fat',
-        'Total Sugars' : 'Sugars',
+        'Total Carbohydrate' : "Carbs",
+        'Sodium' : 'Sodium',
         'Protein' : 'Protein'
 
     }
+    
+    # parses the calories
+    value = nutrition_section.find(string='Calories per serving').find_next('p').text.strip()
+    nutrition_data['Calories'] = value
+
+
+    # ignore
+    value = nutrition_section.find().find()
+
+    # ignore
+    nutrfacts = nutrition_section.find_all('span', class_='nutfactstopnutrient')
+
+
+    rows = nutrition_section.find_all('tr')
 
     for key, label in nutrients.items():
-        curr_elt = nutrition_section.find(text=key)
+
+        for row in rows:
+            curr_elt = row.find(string=key)
+            if curr_elt:
+                value = curr_elt.find_next('p').text.strip()
+                nutrition_data[label] = value
+            
+    # from old push, only gets calories
+    for key, label in nutrients.items():
+        curr_elt = nutrition_section.find_all('tr')
 
         if curr_elt:
             value = curr_elt.find_next('p').text.strip()
@@ -38,8 +60,24 @@ if response.status_code == 200:
         else:
             nutrition_data[label] = 'Not found'
 
+    # loop to print!!
     for key, value in nutrition_data.items():
-        print(f"{key}: {value}")
+            print(f"{key}: {value}")
+
+    """
+    rows = nutrition_section.find_all('tr')
+    for row in rows:
+        nutrients = row.find_all('span', class_='nutfactstopnutrient')
+
+        for nutrient in nutrients:
+            line = nutrient.get_text(strip=True)
+
+            if ' ' in text:
+                name, value = line.split(maxsplit=1)
+                nutrition_data[name.strip()] = value.strip()
+    """
+
+        
 
 else:
     print(f"Failed to access the page. Status code: {response.status_code}")
