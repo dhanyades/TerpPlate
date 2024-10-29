8a71b51f1cf0fd0a67c7bd129b265954253710e9
IsabellaSteinebach <isteineb@terpmail.umd.edu>
2024-10-29 15:54:49 -0400

    Minor style fixes and initial implementation of meal tracking features

--- langchainapp_final/src/main/java/com/example/langchainapp/AssistantImpl.java
+++ langchainapp_final/src/main/java/com/example/langchainapp/AssistantImpl.java
@@ -1,32 +1,12 @@
 package com.example.langchainapp;
 
-import dev.langchain4j.*;
-
 import dev.langchain4j.data.document.*;
 import dev.langchain4j.data.document.parser.TextDocumentParser;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
-import dev.langchain4j.data.message.*;
-import dev.langchain4j.memory.ChatMemory;
-import dev.langchain4j.memory.chat.TokenWindowChatMemory;
-import dev.langchain4j.model.StreamingResponseHandler;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.chat.StreamingChatLanguageModel;
-import dev.langchain4j.model.chat.request.ChatRequest;
-import dev.langchain4j.model.huggingface.HuggingFaceChatModel;
-import dev.langchain4j.model.input.PromptTemplate;
-import dev.langchain4j.model.localai.LocalAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.model.openai.OpenAiTokenizer;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-//import dev.langchain4j.model.ollama.OllamaChatModel;
 import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
-import dev.langchain4j.model.output.Response;
 import dev.langchain4j.service.AiServices;
-import dev.langchain4j.service.SystemMessage;
 import org.springframework.stereotype.Component;
-
-
-import java.util.*;
 
 @Component
 public class AssistantImpl implements NutritionAssistant{
@@ -34,10 +14,12 @@ public class AssistantImpl implements NutritionAssistant{
     public NutritionAssistant assistant;
 
     // Initialize per meal goals based on Average National Standards for an adult consuming around 2k calories every day
-    private String calorieGoal = "600";
-    private String sugarGoal = "600"; //milligrams
-    private String sodiumGoal = "8"; //grams
-    private String proteinGoal = "30"; //grams
+    private String calorieGoal = "650";
+    private String sugarGoal = "10"; //grams
+    private String sodiumGoal = "550"; //milligrams
+    private String proteinGoal = "20"; //grams
+
+    private String menu;
 
 
     public AssistantImpl() {
@@ -46,28 +28,33 @@ public class AssistantImpl implements NutritionAssistant{
                 OpenAiChatModel.builder()
                     .apiKey(System.getenv("OPEN_AI_API_KEY"))
                     .modelName(GPT_4_O_MINI)
+                    .temperature(0.2)
                     .build())
-            //.chatMemory(TokenWindowChatMemory.withMaxTokens(300, new OpenAiTokenizer()))
+
             .build();
+        
+        this.menu = loadMenu();      
     }
 
     public String loadMenu() {
-        Document document = FileSystemDocumentLoader.loadDocument("/Users/isabellasteinebach/Desktop/Technica2024/food_copy.txt", new TextDocumentParser());
+        Document document = FileSystemDocumentLoader.loadDocument("/Users/isabellasteinebach/Desktop/Technica2024/food.txt", new TextDocumentParser());
 
         return document.toTextSegment().text();
     }
 
     public String setMealGoals(String calories, String sugar, String sodium, String protein) {
-        if (calories != null) {
+
+        if (calories != null && !calories.isEmpty())  {
+
             this.calorieGoal = calories;
         }
-        if (sugar != null) {
+        if (sugar != null && !sugar.isEmpty()) {
             this.sugarGoal = sugar;
         }
-        if (sodium != null) {
+        if (sodium != null && !sodium.isEmpty()) {
             this.sodiumGoal = sodium;
         }
-        if (protein != null) {
+        if (protein != null && !protein.isEmpty()) {
             this.proteinGoal = protein;
         }
 
@@ -83,39 +70,63 @@ public class AssistantImpl implements NutritionAssistant{
     }
 
     public String generateMeal() {
-        String menu = loadMenu();
 
-        String systemMessage = "You are here to help college students eat healthy at the dining hall. I will provide you with the daily menu for the specific dining hall that the student is dining in, and you should generate a balanced meal based on the foods in the dining hall." + 
+        String userMessage = "You are here to help college students eat healthy at the dining hall. " + 
+
+        "This particular student's Per Meal Nutritional Goals:\n Calorie Goal (cals): "          + calorieGoal 
+            + "\nSugar Goal (grams): " + sugarGoal
+            + "\nSodium Goal (milligrams): " + sodiumGoal
+            + "\nProtein Goal (grams): " + proteinGoal + 
+            " Create the meal based on these goals." + 
+    
+        "The daily dining hall menu is: " + this.menu
+
+        + "Now that you can see the daily menu for the dining hall, generate a meal based on the menu, so the nutritional facts of each food will add up to meet the students goals. DO NOT generate more than one version of the meal or say that you need to make adjustments, only suggest one meal. \n "
+        
+        + "Return your response in the format: " + 
+
+            "Your Nutritional Goals: \n Calories:[Num calories] \n Sugar: [Sugar] grams \n Sodium:[Sodium] milligrams \n Protein:[Protein] grams" + 
+        
+            "Recommended Meal: \n Food 1:[Name of food] \n Calories:[Num calories (cals)] \n Sugar: [Grams of sugar (g)] \n Sodium:[mg of sodium] \n Protein:[grams of protein] \n\n" + 
+            "Food n:..., Calories:..., Sugar: ...," + 
+
+            "Total Nutrition Facts for the Entire Meal: ...";
+
+        System.out.println("Model getting response");
+        
+        // Generate the response
+        return assistant.chat(userMessage);
+    }
+
+    @Override
+    public String chat(String userMessage) {
+        String chat = "You are here to help college students eat healthy at the dining hall. I will provide you with the daily menu for the specific dining hall that the student is dining in." + 
 
         "This student's Per Meal Nutritional Goals:\n Calorie Goal: "          + calorieGoal 
             + "\nSugar Goal: " + sugarGoal
             + "\nSodium Goal: " + sodiumGoal
             + "\nProtein Goal: " + proteinGoal + 
     
-        "The daily dining hall menu is: " + menu
+        "The daily dining hall menu is: " + this.menu
 
-        + "Please suggest a meal for today's menu. Additionally, please tell me the nutrition facts for each food in a strcutured way. Finally, tell me the total nutrition facts for the entire meal by adding up the numbers from every ingredient.";
+        + "Please answer the students following question:\n " + userMessage;
 
-        //System.out.println(systemMessage);
-
-        System.out.println("Model getting response");
-        
-        // Generate the response
-        String response = assistant.chat(systemMessage);
-
-        System.out.println(response);
-        return response;
+        return assistant.chat(chat);
     }
 
     @Override
-    public String chat(String userMessage) {
+    public String trackMeal(String userMessage) {
+        String chat = "Students want to know the nutritional value of their meal. They will tell you what they are eating, and you will tell them the total nutritional facts of their food based on the menu, and if it meets their goals" + 
 
-        System.out.println("User message is: " + userMessage);
+        "This student's Per Meal Nutritional Goals:\n Calorie Goal: "          + calorieGoal 
+            + "\nSugar Goal: " + sugarGoal
+            + "\nSodium Goal: " + sodiumGoal
+            + "\nProtein Goal: " + proteinGoal + 
+    
+        "The daily dining hall menu is: " + this.menu
 
-        String response = assistant.chat(userMessage);
+        + "This is the student's meal:\n " + userMessage + "\n What are the total nutritional facts of these foods";
 
-        System.out.println("model answer message is: " + response);
-
-        return response;
+        return assistant.chat(chat);
     }
 }
--- langchainapp_final/src/main/java/com/example/langchainapp/WebConfig.java
+++ langchainapp_final/src/main/java/com/example/langchainapp/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-            .allowedOrigins("http://127.0.0.1:5502", "http://127.0.0.1:8080") // Allow your frontend origin
+            .allowedOrigins("http://127.0.0.1:5502", "http://127.0.0.1:8080", "http://127.0.0.1:5500") // Allow your frontend origin
+            // Add the front end port here whatever it is. Look at port plugin.
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow desired HTTP methods
             .allowCredentials(true); // Optional: if you need to allow credentials
     }
